ad978931ec228eefe4f474da8310d6d68dba9032
Sergey Peshkov <zloi.g1rz@gmail.com>
2021-02-07 14:41:53 +0800

    Init

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import './Header.sass';
+
+const Header = () => {
+    return (
+        <header className="header">
+            <div className="container">
+                <a href="/" className="logo">
+                    <span>My</span> gallery
+                </a>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
