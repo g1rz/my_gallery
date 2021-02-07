cc9c50c4d746b8798a3825fa8a81182141ccd0c8
Sergey Peshkov <zloi.g1rz@gmail.com>
2021-02-07 16:06:10 +0800

    Добавил Loader, получение альбомов

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
+import axios from 'axios';
 import React from 'react';
 
 import './App.sass';
 import Header from './components/Header/Header';
+import Loader from './components/Loader/Loader';
 
 function App() {
+    const [albums, setAlbums] = React.useState(null);
+    const [isLoaded, setIsLoaded] = React.useState(false);
+
     React.useEffect(() => {
-        console.log('run');
+        setIsLoaded(false);
+        axios
+            .get('https://jsonplaceholder.typicode.com/albums?_embed=photos&_expand=user&_limit=10')
+            .then(({ data }) => {
+                setAlbums(data);
+                // setIsLoaded(true);
+            });
     }, []);
 
     return (
@@ -14,6 +25,9 @@ function App() {
             <main>
                 <div className="container">
                     <h1>Альбомы</h1>
+                    {isLoaded && <span>Загружено</span>}
+
+                    {!isLoaded && <Loader />}
                 </div>
             </main>
         </div>
@@ -21,3 +35,5 @@ function App() {
 }
 
 export default App;
+
+// https://jsonplaceholder.typicode.com/albums?_embed=photos&_expand=user&_limit=10
