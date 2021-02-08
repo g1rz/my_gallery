4c5bc563a53477bfd422a09dc9937ba58701ce8f
Sergey Peshkov <zloi.g1rz@gmail.com>
2021-02-08 17:32:00 +0800

    Переименовал UserAlbums в UserPage

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import './App.sass';
 
 import Header from './components/Header/Header';
 import Home from './pages/Home/Home';
-import UserAlbums from './pages/UserAlbums/UserAlbums';
+import UserAlbums from './pages/UserPage/UserPage';
 import AlbumPage from './pages/AlbumPage/AlbumPage';
 
 function App() {
