fc1abcb05884c49aaf16da48ceb7892a20ddea92
Sergey Peshkov <zloi.g1rz@gmail.com>
2021-02-08 17:02:19 +0800

    Добавил альбомам ссылки на их страницы

--- src/components/Album/Album.jsx
+++ src/components/Album/Album.jsx
@@ -1,23 +1,21 @@
-import React from 'react'
-
-import './Album.sass'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Album.sass';
 
 const Album = ({ id, cover, title, photosCount }) => {
     return (
         <div className="album-wrap">
-            <div className="album" key={id}>
-                <a href="/" className="album__img-wrap">
+            <Link to={`/album/${id}`} className="album">
+                <p className="album__img-wrap">
                     <img src={cover.thumbnailUrl} alt={cover.title} className="album__img" />
-                </a>
+                </p>
                 <div className="album__body">
                     <p>
-                        <a href="/" className="album__title-link">{title}</a>
+                        {title}
                         <span className="album__count">({photosCount})</span>
                     </p>
-
-
                 </div>
-            </div>
+            </Link>
         </div>
 
     )
