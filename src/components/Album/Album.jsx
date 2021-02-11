aef32e03d04fbd86796c19a6988cfd174dfceaa6
Sergey Peshkov <zloi.g1rz@gmail.com>
2021-02-12 01:22:31 +0800

    Добавил скелетон для обложки альбома

--- src/components/Album/Album.jsx
+++ src/components/Album/Album.jsx
@@ -3,11 +3,18 @@ import { Link } from 'react-router-dom';
 import './Album.sass';
 
 const Album = ({ id, cover, title, photosCount }) => {
+    const [isLoaded, setIsLoaded] = React.useState(false);
     return (
         <div className="album-wrap">
             <Link to={`/album/${id}`} className="album">
                 <p className="album__img-wrap">
-                    <img src={cover.thumbnailUrl} alt={cover.title} className="album__img" />
+                    {!isLoaded && <span className="album__img-skeleton"></span>}
+                    <img
+                        src={cover.thumbnailUrl}
+                        alt={cover.title}
+                        className="album__img"
+                        onLoad={() => setIsLoaded(true)}
+                    />
                 </p>
                 <div className="album__body">
                     <p>
@@ -17,8 +24,7 @@ const Album = ({ id, cover, title, photosCount }) => {
                 </div>
             </Link>
         </div>
+    );
+};
 
-    )
-}
-
-export default Album
+export default Album;
