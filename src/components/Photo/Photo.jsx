a41123ac5445e43ff492768dfb2f02258802b1b1
Sergey Peshkov <zloi.g1rz@gmail.com>
2021-02-11 17:56:11 +0800

    Добавил скелетон до загрузки фотографий

--- src/components/Photo/Photo.jsx
+++ src/components/Photo/Photo.jsx
@@ -2,13 +2,22 @@ import React from 'react';
 // import { Link, useLocation } from 'react-router-dom';
 
 import './Photo.sass';
+import PhotoSkeleton from './PhotoSkeleton';
 
 const Photo = ({ id, title, thumbnailUrl, openModal }) => {
+
+    const [isLoaded, setIsLoaded] = React.useState(false);
+
+    const handleLoad = () => {
+        setIsLoaded(true)
+    }
 
     return (
         <div className="photo-wrap">
             <div className="photo" onClick={() => openModal(id)}>
-                <img src={thumbnailUrl} alt={title} className="photo__img" width="150" height="150" />
+                {!isLoaded && <PhotoSkeleton />}
+                <img src={thumbnailUrl} alt={title} className="photo__img" onLoad={() => handleLoad()} />
+
             </div>
         </div>
     );
---
+++ src/components/Photo/PhotoSkeleton.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+const PhotoSkeleton = () => {
+    return (
+        <div className="photo-skeleton"></div>
+    )
+}
+
+export default PhotoSkeleton;
